56d39a4e5637652380d9fbe7ecec2588145bdcca
hardikphalet <hardik.phalet@captainfresh.in>
2024-07-22 04:57:42 +0530

    - Added list handling in BinaryFilter

--- src/main/java/org/trips/service_framework/utils/search/filters/BinaryFilter.java
+++ src/main/java/org/trips/service_framework/utils/search/filters/BinaryFilter.java
@@ -5,11 +5,14 @@ import lombok.Data;
 import org.trips.service_framework.utils.search.Filter;
 import org.trips.service_framework.utils.search.operators.BinaryOperator;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 /**
  * @author : hardikphalet
  * @since : 22/07/24
  **/
-
 @Data
 @Builder
 public class BinaryFilter<T> implements Filter {
@@ -19,6 +22,10 @@ public class BinaryFilter<T> implements Filter {
 
     @Override
     public String getQuery() {
-        return String.format("%s.%s:%s", field, operation.getOperation(), value);
+        Optional<String> formattedValue = Optional.empty();
+        if (value instanceof List) {
+            formattedValue = Optional.of(((List<?>) value).stream().map(Object::toString).collect(Collectors.joining(",")));
+        }
+        return String.format("%s.%s:%s", field, operation.getOperation(), formattedValue.orElse(value.toString()));
     }
 }
